0f7d56e532b9af8894df62e770b4a2c43febdd1b
Renato Vásquez <rena_vasquez@icloud.com>
2023-10-06 21:02:21 -0300

    Add files via upload

---
+++ FPI/Guía 00/De_Suma_a_Multiplicacion.py
@@ -0,0 +1,12 @@
+# Entrada
+entrada_1 = input("Primer valor: ")
+entrada_2 = input("Segundo valor: ")
+
+# Procesamiento
+x_1 = int(entrada_1)
+x_2 = int(entrada_2)
+
+resultado = x_1 * x_2
+
+# Salida
+print("El resultado es", resultado)
---
+++ FPI/Guía 00/Practica_Tarea_1.py
@@ -0,0 +1,9 @@
+# Programa de lectura de precios y descuentos, entregando cantidad de dinero ahorado, total a pagar, cantidad de puntos acumulados y un mensaje de despedida cuando el programa finaliza.
+
+# Entrada
+
+
+# Procesamiento
+
+
+# Salida
