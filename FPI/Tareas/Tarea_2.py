40e579fc543c9d01a8f407a6df96aaa9c69de7a3
Renato Vásquez <rena_vasquez@icloud.com>
2023-11-16 18:16:05 -0300

    Add files via upload

--- FPI/Tareas/Tarea_2.py
+++ FPI/Tareas/Tarea_2.py
@@ -1,5 +1,4 @@
 ### Definiciones auxiliares
-# Definimos una definición auxiliar para la Definición 1 solicitada
 def posicion_en_tablero(pos):
   columna = pos[0]
   fila = int(pos[1])
@@ -8,21 +7,16 @@ def posicion_en_tablero(pos):
 
 
 ## Definición 1: validar la posición
-# Se empieza la Definición 1 solicitada
 def validar_posicion(pos, tablero):
-  # Definimos columnas y filas dentro del tablero con la posición dada
   columnas_val = "ABCDEFGH"
   columna, fila = posicion_en_tablero(pos)
 
-  # Verifico que la posición sea valida
   if columna not in columnas_val or fila < 1 or fila > 8:
     return -1
 
-  # Definimos filas y columnas en el tablero
   fila_in = 8 - fila
   columna_in = columnas_val.index(columna)
 
-  # Verificamos que si la posicion en tablero esta ocupada
   if tablero[fila_in][columna_in] != "  ":
     return 1
 
@@ -30,7 +24,6 @@ def validar_posicion(pos, tablero):
 
 
 ## Definición 2: imprimir tablero
-# Creamos el tablero para verificar las posiciones e incluirlas en la impresión
 def entregar_tablero(piezas):
   tablero = []
   for _ in range(8):
@@ -39,10 +32,8 @@ def entregar_tablero(piezas):
       fila.append("  ")
     tablero.append(fila)
 
-  # Separamos los elementos que nos estreguen
   elementos = piezas.split(",")
 
-  # Defino a que cosa corresponde cada dato entregado
   for elemento in elementos:
     if len(elemento) == 4:
       color = elemento[0]
@@ -50,27 +41,19 @@ def entregar_tablero(piezas):
       columna = elemento[2]
       fila = elemento[3]
 
-      # Verifico que la posición este dentro de estos valores
-      if color in ("B", "N") and pieza in (
-          'P', 'T', 'C', 'A', 'D',
-          'R') and columna in ('A', 'B', 'C', 'D', 'E', 'F', 'G',
-                               'H') and fila in ('1', '2', '3', '4', '5', '6',
-                                                 '7', '8'):
+      if color in ("B", "N") and pieza in ('P', 'T', 'C', 'A', 'D','R') and columna in ('A', 'B', 'C', 'D', 'E', 'F', 'G','H') and fila in ('1', '2', '3', '4', '5', '6','7', '8'):
         fila_ind = 8 - int(fila)
         columna_ind = ord(columna) - ord("A")
         pieza_y_color = color + pieza
 
-        # Verificamos que si el espacio en tablero esta disponible le imprima la posicion y pieza correspondiente
         if tablero[fila_ind][columna_ind] == "  ":
           tablero[fila_ind][columna_ind] = pieza_y_color
 
-  # Retornamos el tablero
   return tablero
 
 
 ## Definición 3: validación de juego
 def es_valido(tablero):
-  # Generamos contadores de piezas en el tablero
   cont_blancas_p = 0
   cont_blancas_t = 0
   cont_blancas_c = 0
@@ -98,15 +81,12 @@ def es_valido(tablero):
 
   for fila_ind in range(8):
     for columna_ind in range(8):
-      # Defino la variable pieza para hacer mas sencillo el buscar las piezas
       pieza = tablero[fila_ind][columna_ind]
 
-      # Empiezo el ciclo de busqueda y de sumar al contador
       if pieza != "  ":
         jugador = pieza[0]
         tipo_pieza = pieza[1]
 
-        # Contabilizo las piezas Blancas
         if jugador == "B":
           if tipo_pieza == "P":
             cont_blancas_p += 1
@@ -124,7 +104,6 @@ def es_valido(tablero):
           if tipo_pieza == "R":
             rey_blanco_pres = True
 
-          # Verifico la posicion y si es peon negro o blanco
           if jugador == "B" and tipo_pieza == "A":
             if (fila_ind + columna_ind) % 2 == 0:
               alfil_blanco_blanco = True
@@ -169,8 +148,7 @@ def es_valido(tablero):
                         and cont_blancas_a <= 2 and cont_negras_a <= 2
                         and cont_blancas_r == 1 and cont_negras_r == 1)
 
-  condiciones_alfiles_fila = (alfil_blanco_blanco and alfil_blanco_negra
-                              and alfil_negro_negro and alfil_negro_blanco)
+  condiciones_alfiles_fila = (alfil_blanco_blanco and alfil_blanco_negra and alfil_negro_negro and alfil_negro_blanco)
 
   condiciones_peones_fila = peones_blancos_filas_correctas and peones_negros_filas_correctas
 
@@ -206,16 +184,17 @@ def encontrar_posicion(pieza, tablero):
 
   return None
 
+
 # Definición para determinar si el rey está en jaque
 def rey_en_jaque(posicion_rey, oponente, tablero):
   for fila_ind in range(8):
     for columna_ind in range(8):
       pieza = tablero[fila_ind][columna_ind]
-      if pieza[0] == oponente and es_movimiento_valido(
-          pieza, (fila_ind, columna_ind), posicion_rey):
+      if pieza[0] == oponente and es_movimiento_valido(pieza, (fila_ind, columna_ind), posicion_rey):
         return True
 
   return False
+
 
 # Definición para determinar si el movimiento es válido
 def es_movimiento_valido(pieza, origen, destino):
@@ -233,6 +212,7 @@ def es_movimiento_valido(pieza, origen, destino):
   elif tipo_pieza == "R":
     return es_movimiento_rey(origen, destino)
 
+
 ### Definciónes auxiliares para def es_movimiento_valido
 # Definición para validar movimiento del peon
 def es_movimiento_peon(color, origen, destino):
@@ -240,11 +220,10 @@ def es_movimiento_peon(color, origen, destino):
   fila_destino, columna_destino = destino
 
   if color == "B":
-    return fila_destino == fila_origen - 1 and abs(columna_destino -
-                                                   columna_destino) == 1
+    return fila_destino == fila_origen - 1 and abs(columna_destino - columna_destino) == 1
   else:
-    return fila_destino == fila_origen + 1 and abs(columna_destino -
-                                                   columna_origen) == 1
+    return fila_destino == fila_origen + 1 and abs(columna_destino - columna_origen) == 1
+
 
 # Definición para el movimiento de la torre
 def es_movimiento_torre(origen, destino):
@@ -253,33 +232,31 @@ def es_movimiento_torre(origen, destino):
 
   return fila_origen == fila_destino or columna_origen == columna_destino
 
+
 # Definición para el movimiento del caballo
 def es_movimiento_caballo(origen, destino):
   fila_origen, columna_origen = origen
   fila_destino, columna_destino = destino
 
-  return (abs(fila_destino - fila_origen) == 2
-          and abs(columna_destino - columna_origen)
-          == 1) or (abs(columna_destino - columna_origen) == 2
-                    and abs(fila_destino - fila_origen) == 1)
+  return (abs(fila_destino - fila_origen) == 2 and abs(columna_destino - columna_origen) == 1) or (abs(columna_destino - columna_origen) == 2 and abs(fila_destino - fila_origen) == 1)
+
 
 # Definición para el movimiento del alfil
 def es_movimiento_alfil(origen, destino):
   fila_origen, columna_origen = origen
   fila_destino, columna_destino = destino
 
-  return abs(fila_destino - fila_origen) == abs(columna_destino -
-                                                columna_origen)
+  return abs(fila_destino - fila_origen) == abs(columna_destino - columna_origen)
+
 
 # Definición para el movimiento de la reina
 def es_movimiento_dama(origen, destino):
-  return es_movimiento_torre(origen, destino) or es_movimiento_alfil(
-      origen, destino)
+  return es_movimiento_torre(origen, destino) or es_movimiento_alfil(origen, destino)
+
 
 # Definición para el movimiento del rey
 def es_movimiento_rey(origen, destino):
   fila_origen, columna_origen = origen
   fila_destino, columna_destino = destino
 
-  return abs(fila_destino - fila_origen) <= 1 and abs(columna_destino -
-                                                      columna_origen) <= 1
+  return abs(fila_destino - fila_origen) <= 1 and abs(columna_destino - columna_origen) <= 1
