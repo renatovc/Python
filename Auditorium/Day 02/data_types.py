d8bc2ee28e8cc7951a0f0dd366e09ac79d69d259
Renato Vásquez <rena_vasquez@icloud.com>
2024-05-30 19:49:32 -0400

    Add files via upload

---
+++ Auditorium/Day 02/data_types.py
@@ -0,0 +1,8 @@
+two_digit_number = input()
+
+firs_digit = int(two_digit_number[0])
+second_digit = int(two_digit_number[1])
+
+suma = firs_digit + second_digit
+
+print(suma)
---
+++ Auditorium/Day 02/life_in_weeks.py
@@ -0,0 +1,9 @@
+age = input()
+
+age = int(age)
+
+weeks_to_90 = 4680
+
+weeks = weeks_to_90 - (age * 52)
+
+print(f"You have {weeks} weeks left.")
